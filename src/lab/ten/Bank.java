4ab9a6fb3142eb57201d785012fc8378f608a79a
cryptoAlgorithm <hi@vinkwok.tk>
2022-03-29 21:31:33 +0800

    Finish lab 10 qn 3, originally missed that out

---
+++ src/lab/ten/Bank.java
@@ -0,0 +1,56 @@
+package lab.ten;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    //declare an ArrayList of BankAccount objects
+    private final ArrayList<BankAccount> accounts;
+
+    public Bank() {
+
+        //create ArrayList
+        accounts = new ArrayList<>();
+    }
+
+    //add an account to this bank
+    public void addAccount(BankAccount a) {
+        accounts.add(a);
+    }
+
+    //Gets the sum of the balances of all accounts in this bank
+    public double getTotalBalance() {
+        double total =0;
+        for(BankAccount a: accounts) {
+            total = total + a.getBalance();
+        }
+        return total;
+    }
+
+    public int count(int bal) {
+        int s = 0;
+        for (BankAccount a: accounts) {
+            if (a.getBalance() >= bal) s++;
+        }
+        return s;
+    }
+
+    public BankAccount find(int acctNumber) {
+        for (BankAccount a: accounts) {
+            if (a.getAccountNumber() == acctNumber) return a;
+        }
+        return null;
+    }
+
+    public int largest() {
+        double m = -1;
+        int d = 0;
+        for (BankAccount a: accounts) {
+            if (a.getBalance() > m) {
+                m = a.getBalance();
+                d = a.getAccountNumber();
+            }
+        }
+        return d;
+    }
+}
---
+++ src/lab/ten/ReverseList.java
@@ -0,0 +1,33 @@
+package lab.ten;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = s.nextInt();
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nums.add((int) (Math.floor(Math.random() * 101) - 50));
+        }
+        System.out.println("Contents of Original Array:");
+        for (Integer i: nums) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Contents of Reversed Array:");
+        for (Integer i: reverseList(nums)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static ArrayList<Integer> reverseList(ArrayList<Integer> a) {
+        ArrayList<Integer> na = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++) {
+            na.add(a.get(a.size() - i - 1));
+        }
+        return na;
+    }
+}
